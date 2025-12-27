7df37dd165a0446ead0b738c88850207b8e1dfba
vishal <vishalray7891@gmail.com>
2025-12-28 01:57:51 +0530

    Update productModel.js

--- project-backend/models/productModel.js
+++ project-backend/models/productModel.js
@@ -1,19 +1,48 @@
+// const mongoose = require("mongoose");
+
+// const productSchema = new mongoose.Schema(
+//   {
+//     title: { type: String, required: true },
+//     image: { type: String }, 
+//     price: { type: Number, required: true },
+//     category: { type: String, required: true },
+//     categorySlug: { type: String, required: false },
+//     subcategory: { type: String, required: false },
+//     subCategorySlug: { type: String, required: false },
+//     inStock: { type: Boolean, default: true },
+//     isFeatured: { type: Boolean, default: false },
+//   },
+//   { timestamps: true }
+//   collection: "products" //  YE LINE FIX HAI
+// );
+
+// module.exports = mongoose.model("Product", productSchema);
+
+
+
+
+// updated
+
+
+
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    image: { type: String }, 
+    image: { type: String },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    categorySlug: { type: String, required: false },
-    subcategory: { type: String, required: false },
-    subCategorySlug: { type: String, required: false },
+    categorySlug: { type: String },
+    subcategory: { type: String },
+    subCategorySlug: { type: String },
     inStock: { type: Boolean, default: true },
     isFeatured: { type: Boolean, default: false },
   },
-  { timestamps: true }
-  collection: "products" //  YE LINE FIX HAI
+  {
+    timestamps: true,
+    collection: "products", // YAHAN SAHI HAI
+  }
 );
 
 module.exports = mongoose.model("Product", productSchema);
