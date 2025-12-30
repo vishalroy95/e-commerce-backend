3dc3ea7b1e28075f97c0d370f1a71d6a794d564d
vishal <vishalray7891@gmail.com>
2025-12-31 00:19:01 +0530

    Update db.js

--- project-backend/config/db.js
+++ project-backend/config/db.js
@@ -1,8 +1,38 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//     console.log('Connected DB:', mongoose.connection.name);
+//   } catch (err) {
+//     console.error('MongoDB connection error:', err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+// updated 
+
+
+
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
   try {
+
+    // ✅ YE LINE ADD KAR
+    mongoose.set("strictQuery", false);
+
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
